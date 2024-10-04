ac0190c5d4826b2f9eab8a0cc5e097667db9b6c0
Ayhan Kırıkcı <ayhankirikci2264@gmail.com>
2024-10-04 18:45:27 +0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import localFont from "next/font/local";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function Home() {
+  return (
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+      <h1 className="text-secondary-900">Testasd adsasdasd as dasdasd</h1>
+      <main className="">
+        {/* <Image
+          className="dark:invert"
+          src="https://nextjs.org/icons/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        /> */}
+      </main>
+      <footer className=""></footer>
+    </div>
+  );
+}
